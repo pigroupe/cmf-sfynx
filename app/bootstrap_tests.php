d2a9e68972f5d32d030e486dca34c45d3cca8fe0
alhassane <etienne.delongeaux@gmail.com>
2015-01-14 16:42:57 +0100

    add bootstrap for phpunit tests

---
+++ app/bootstrap_tests.php
@@ -0,0 +1,14 @@
+<?php
+use Symfony\Component\Process\PhpProcess;
+
+require_once __DIR__ . '/bootstrap.php.cache';
+
+Phake::setClient(Phake::CLIENT_PHPUNIT);
+
+$process = new PhpProcess('php app/console doctrine:database:drop --force --env=test');
+$process->run();
+
+$process = new PhpProcess('php app/console doctrine:database:create --env=test');
+$process->run();
+
+$process = new PhpProcess('php app/console doctrine:schema:create --env=test');
