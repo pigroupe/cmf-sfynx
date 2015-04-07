0a73a25e1ab8ce29de8bbab7214c95e8c0a7acda
alhassane <etienne.delongeaux@gmail.com>
2015-04-07 18:02:57 +0200

    test

--- sysadmin/scriptshell/installer-sfynx.sh
+++ sysadmin/scriptshell/installer-sfynx.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 if [ ! -f composer.phar ]; then
     wget https://getcomposer.org/composer.phar -O ./composer.phar
+    php -d memory_limit=1024M composer.phar install --no-interaction
+    php composer.phar dump-autoload --optimize
 fi
 
 #
+mkdir -p app/cache
+mkdir -p app/logs
 mkdir -p app/cachesfynx/loginfailure
 mkdir -p web/uploads/media
-mkdir web/yui
+mkdir -p web/yui
 
 sudo chmod -R 0777 app/cachesfynx
 sudo chmod -R 0777 app/cache
