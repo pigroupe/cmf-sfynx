cef25df248ebd3070cc7f522d9517c9e29223ec8
alhassane <etienne.delongeaux@gmail.com>
2015-06-09 16:44:45 +0200

    debug stack

--- provisioners/shell/phpcr-browser/installer-browser.sh
+++ provisioners/shell/phpcr-browser/installer-browser.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
+DIR=$1
+
 PLATEFORM_PROJET_NAME=phpcr-browser
 
 #Web interface for browsing PHPCR repositories, using Silex and AngularJS 
 #https://github.com/marmelab/phpcr-browser
 
-DIR=$1
-INSTALL_USERWWW=$2
-
 echo "*****Install Web interface for browsing PHPCR repositories, using Silex and AngularJS"
-
-mkdir -p $DIR/phpcr-browser
-
+mkdir -p /tmp
+mkdir -p /websites/phpcr-browser
+sudo chmod -R 777 /websites/phpcr-browser
 cd /tmp
+
 echo "***** Clone the repository "
 sudo git clone https://github.com/marmelab/phpcr-browser
 cd phpcr-browser
-cp -r * $DIR/phpcr-browser
+sudo cp -r * /websites/phpcr-browser
+cd /websites/phpcr-browser
 
-cd $DIR/phpcr-browser
-
-# we install the composer file
-if [ ! -f composer.phar ]; then
-    wget https://getcomposer.org/composer.phar -O ./composer.phar
-    # curl -s https://getcomposer.org/installer | php
+echo "***** We configure phpcr-browser "
+if [ ! -f config/prod.yml ]; then
+    cp config/prod.yml-dist config/prod.yml
+    sed -i 's/8080/8081/g' config/prod.yml
 fi
-echo "***** Install dependencies and configure the browser"
-php -d memory_limit=1024M composer.phar install --no-interaction
 
 # we create the virtualhiost of sfynx for nginx
-mkdir -p /tmp
 cat <<EOT >/tmp/$PLATEFORM_PROJET_NAME
 #upstream php5-fpm-sock {  
 #    server unix:/var/run/php5-fpm.sock;  
 #}
 
 server {
-    set \$website_root "$INSTALL_USERWWW/$PLATEFORM_PROJET_NAME/web";
+    set \$website_root "/websites/phpcr-browser/web";
     set \$default_env  "index.php";
 
     listen 80;
@@ -49,24 +45,10 @@ server {
     # Don't show the nginx version number, a security best practice
     server_tokens off;
 
-    # charset
-    charset utf-8;
-
     # Logging
-    access_log off; 
-    log_not_found off; 
-    #error_log  /var/log/nginx/sfynx-error.log;
-
-    # Adjust output buffers
-    fastcgi_buffers 256 16k; 
-    fastcgi_buffer_size 128k; 
-    fastcgi_connect_timeout 3s; 
-    fastcgi_send_timeout 120s; 
-    fastcgi_read_timeout 120s; 
-    fastcgi_busy_buffers_size 256k; 
-    fastcgi_temp_file_write_size 256k; 
-    reset_timedout_connection on; 
-    #server_names_hash_bucket_size 100;
+    access_log on; 
+    #log_not_found off; 
+    #error_log  /var/log/nginx/phpcrbrowser-error.log;
 
     location / {
         # try to serve file directly, fallback to rewrite
@@ -79,13 +61,11 @@ server {
     }
 
     # Pass the PHP scripts to FastCGI server
-    location ~ ^/index\.php(/|\$) {
+    location ~ ^/(index)\.php(/|\$) {
         fastcgi_pass php5-fpm-sock;
-        fastcgi_split_path_info ^(.+\.php)(/.*)\$;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         fastcgi_param  HTTPS off;
-       
     }
 
     # Nginx Cache Control for Static Files
@@ -101,9 +81,7 @@ server {
         log_not_found off; 
         deny all; 
     }
-
 }
-
 EOT
 sudo mv /tmp/$PLATEFORM_PROJET_NAME /etc/nginx/sites-available/$PLATEFORM_PROJET_NAME
 
@@ -111,10 +89,21 @@ sudo mv /tmp/$PLATEFORM_PROJET_NAME /etc/nginx/sites-available/$PLATEFORM_PROJET
 sudo ln -s /etc/nginx/sites-available/$PLATEFORM_PROJET_NAME /etc/nginx/sites-enabled/$PLATEFORM_PROJET_NAME
 
 # we add host in the /etc/hosts file
-if ! grep -q "www.$PLATEFORM_PROJET_NAME.local" /etc/hosts; then
+if ! grep -q "dev.$PLATEFORM_PROJET_NAME.local" /etc/hosts; then
     echo "Adding hostname to your /etc/hosts"
     echo "127.0.0.1    www.$PLATEFORM_PROJET_NAME.local" | tee --append /etc/hosts
 fi
+
+# we install the composer file
+if [ ! -f composer.phar ]; then
+    wget https://getcomposer.org/composer.phar -O ./composer.phar
+    # curl -s https://getcomposer.org/installer | php
+fi
+#composer require jackalope/jackalope-doctrine-dbal:1.1.* --no-update
+#composer update jackalope/jackalope-doctrine-dbal
+
+echo "***** Install project"
+make install
 
 # we restart nginx server
 sudo service nginx restart
